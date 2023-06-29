a5ef85d6f6b192804cc9cd17f6cc538e9c6d3998
Emerson S <eyenessus>
2023-06-29 11:33:45 -0300

    barra de progresso

--- 4galerialightbox/script.js
+++ 4galerialightbox/script.js
@@ -4,9 +4,7 @@ const lightboxItem = document.querySelector('.lightbox-image')
 const lightboxClose = document.querySelector('.lightbox-close')
 galleryItem.forEach((item) => {
     item.addEventListener('click', () => {
-        console.log(item)
         const imageUrl = item.querySelector('.gallery-image').getAttribute("data-src")
-        console.log(imageUrl)
         lightboxItem.setAttribute("src",imageUrl)
         lightbox.style.display = "flex";
     });
